98a59173668f6198e4118bdbfba399e8de951370
hanyanzhao <championhan@163.com>
2014-01-21 11:47:26 +0800

    颍上的屏是9块P10板子宽，10块高

--- Preincludes/led_huaibei_p10.h
+++ Preincludes/led_huaibei_p10.h
@@ -10,7 +10,7 @@
 #define __TARGET_STRING__               "LED_LIXIN_288"
 
 #define USE_QIANGLI_P10_1R               1
-#define QIANGLI_UNIT_X_NUM               10
+#define QIANGLI_UNIT_X_NUM               9
 #define QIANGLI_UNIT_Y_NUM               8
 
 #else
